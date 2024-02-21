c5ad8ef699d9446ab9d438aa39e7647fbd439395
Fauzan Muhammad Muharam <fmuharam25@gmail.com>
2024-02-22 06:52:37 +0700

    chore : routes

---
+++ api/routes/department.go
@@ -0,0 +1,15 @@
+package routes
+
+import (
+	handlers "github.com/fmuharam25/tutorial-golang-gofiber/api/handlres"
+	"github.com/fmuharam25/tutorial-golang-gofiber/pkg/department"
+	"github.com/gofiber/fiber/v2"
+)
+
+// DepartmentRouter is the Router for GoFiber App
+func DepartmentRouter(app fiber.Router, service department.Service) {
+	app.Get("/departments", handlers.GetDepartments(service))
+	app.Post("/departments", handlers.AddDepartment(service))
+	app.Put("/departments", handlers.UpdateDepartment(service))
+	app.Delete("/departments", handlers.RemoveDepartment(service))
+}
---
+++ api/routes/employee.go
@@ -0,0 +1,15 @@
+package routes
+
+import (
+	handlers "github.com/fmuharam25/tutorial-golang-gofiber/api/handlres"
+	"github.com/fmuharam25/tutorial-golang-gofiber/pkg/employee"
+	"github.com/gofiber/fiber/v2"
+)
+
+// EmployeeRouter is the Router for GoFiber App
+func EmployeeRouter(app fiber.Router, service employee.Service) {
+	app.Get("/employees", handlers.GetEmployees(service))
+	app.Post("/employees", handlers.AddEmployee(service))
+	app.Put("/employees", handlers.UpdateEmployee(service))
+	app.Delete("/employees", handlers.RemoveEmployee(service))
+}
