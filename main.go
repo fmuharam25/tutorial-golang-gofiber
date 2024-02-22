0fccc4dd134a80fffa85e7ed02d2a8bbfbe5e3bd
Fauzan Muhammad Muharam <fmuharam25@gmail.com>
2024-02-22 07:00:41 +0700

    chore : WIP main connection

--- main.go
+++ main.go
@@ -1,13 +1,56 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/fmuharam25/tutorial-golang-gofiber/api/routes"
+	"github.com/fmuharam25/tutorial-golang-gofiber/pkg/department"
+	"github.com/fmuharam25/tutorial-golang-gofiber/pkg/employee"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
 
 func main() {
-	app := fiber.New()
+	db, cancel, err := databaseConnection()
+	if err != nil {
+		log.Fatal("Database Connection Error $s", err)
+	}
+	fmt.Println("Database connection success!")
+	departmentCollection := db.Collection("departments")
+	departmentRepo := department.NewRepo(departmentCollection)
+	departmentService := department.NewService(departmentRepo)
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+	employeeCollection := db.Collection("employees")
+	employeeRepo := employee.NewRepo(employeeCollection)
+	employeeService := employee.NewService(employeeRepo)
+
+	//Welcome Routes
+	app := fiber.New()
+	app.Use(cors.New())
+	app.Get("/", func(ctx *fiber.Ctx) error {
+		return ctx.Send([]byte("Welcome to the API V1 Department and Employee!"))
 	})
 
-	app.Listen(":3000")
+	//API Route
+	api := app.Group("/api")
+	routes.DepartmentRouter(api, departmentService)
+	routes.EmployeeRouter(api, employeeService)
+	defer cancel()
+	log.Fatal(app.Listen(":8080"))
+}
+
+func databaseConnection() (*mongo.Database, context.CancelFunc, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://username:password@localhost:27017/fiber").SetServerSelectionTimeout(5*time.Second))
+	if err != nil {
+		cancel()
+		return nil, nil, err
+	}
+	db := client.Database("departments")
+	return db, cancel, nil
 }
